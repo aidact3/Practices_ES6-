6c5a43dc964bd6e7f62a5004dfe90f627b6db2bf
aidact3 <aidacarabot.tosar@gmail.com>
2024-04-21 10:05:28 -0400

    ejercicio10

--- ejercicio9.js
+++ ejercicio9.js
@@ -1,4 +1,4 @@
-// Dado el siguiente javascript usa forof para recorrer el array de películas, 
+//! Dado el siguiente javascript usa forof para recorrer el array de películas, 
 // genera un nuevo array con las categorías de las películas e imprime por 
 // consola el array de categorías. Ten en cuenta que las categorías no deberían 
 // repetirse.
