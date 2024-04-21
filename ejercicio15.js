ca45c100b934723fe8e70c045a60c8d8d7038512
aidact3 <aidacarabot.tosar@gmail.com>
2024-04-21 18:59:12 -0400

    ejercicio15

---
+++ ejercicio15.js
@@ -0,0 +1,25 @@
+// Crea una función llamada swap que reciba un array y dos parametros que sean 
+// indices del array. La función deberá intercambiar la posición de los valores de 
+// los indices que hayamos enviado como parametro. Retorna el array resultante.
+
+// Sugerencia de array:
+
+const fantasticFour = [
+  "La antorcha humana",
+  "Mr. Fantástico",
+  "La mujer invisible",
+  "La cosa",
+];
+
+function swap(array, index1, index2) {
+  // Intercambiar los valores en los índices especificados
+  const temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+
+  // Retornar el array resultante
+  return array;
+}
+
+// Ejemplo de uso
+console.log(swap(fantasticFour, 1, 3)); // Intercambiar "Mr. Fantástico" y "La cosa"
